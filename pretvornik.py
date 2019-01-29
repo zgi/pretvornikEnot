ea4f8307bede158bda240ea0413e41c969690974
Gregor Žagar <gregor.zagar@gmail.com>
2019-01-29 15:23:37 +0100

    second try

---
+++ pretvornik.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+print "Pozdravljeni. Sem pretvornik enot, kilometrov v milje. " \
+      "\nVpiši željene kilometre in pretvoril jih bom v kilometre."
+
+def pretvornik():
+    try:
+        kilometri = float(raw_input("\nVpiši kilometre: "))
+        milje = round(kilometri * 0.621371192, 1)
+        print "Vpisal si" ,kilometri, " kilometrov, kar je približno ", milje," milj"
+    except ValueError:
+        print "Napačen vnos! Poskusi ponovno, tokrat s številko..."
+    ponoviVajo = str(raw_input("Ali želiš še kaj pretvoriti? (da/ne)"))
+    while ponoviVajo == "da":
+        pretvornik()
+        break
+pretvornik()
